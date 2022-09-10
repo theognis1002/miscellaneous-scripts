c26e238b79f68bd52088cca42fce27fe4804e698
theognis1002 <theognis1002@gmail.com>
2022-09-10 18:59:11 -0400

    Add system_update.sh

---
+++ system_uptime.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+upserver=$(for server in `cat /tmp/servers.txt`
+do nc -zvw3 $server 22 | awk '/succeeded/ {print $3}'
+done 2>/dev/null)
+for host in $upserver
+do
+echo -n "$host: "
+ssh $host uptime | awk '{print $3,$4}' | sed 's/,//'
+done | column -t > /tmp/uptime-report.out
+cat /tmp/uptime-report.out | mail -s "Linux Servers Uptime Report" "daygeek@gmail.com"
